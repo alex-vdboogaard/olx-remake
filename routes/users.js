a53720aee4af395a9b95d9236d721e3b4d2b7883
Alex van den Boogaard <alex.vandenboogaard@outlook..com>
2024-04-18 12:38:28 +0200

    fix /user middleware

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,13 @@
 //only registered users  
 const express = require("express");
 const router = express.Router();
-
+router.use((req, res, next) => {
+    if (!req.session.user_id) {
+        req.flash("danger", "Please login to access your dashboard")
+        return res.redirect("/login");
+    }
+    next();
+})
 router.get("/dashboard", (req,res) => {
     res.render("dashboard", { user_id: req.session.user_id })
 })
@@ -18,13 +24,6 @@ router.get("/dashboard/create-listing", (req, res) => {
 
 router.get("/dashboard/listing", async (req, res) => {
     res.render("editListing", {user_id: req.session.user_id, listing_id:req.query.id});
-})
-router.use("/user", (req, res, next) => {
-    if (!req.session.user_id) {
-        req.flash("danger", "Please login to access your dashboard")
-        return res.redirect("/login");
-    }
-    next();
 })
 
 module.exports = router;
